cf0297c22cb9dcd66f9e4ca6c78dfd6b0076c678
amishranpariya <61049238+AmishRanpariya@users.noreply.github.com>
2020-07-23 17:13:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,25 +4,31 @@ var x, y, sx,
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  // starting position and speed,
   x = int(random(r, width - r));
   y = int(random(r, height - r));
   sy = random(5, 24);
   sx = random(5, 24);
+  
+  fill(0);
+  stroke(255,0,255);
+  strokeWeight(r / 20);
 }
 
 function draw() {
 
   background(119, 250, 125);
-  fill(0);
-  stroke(255,0,255);
-  strokeWeight(r / 20);
+  
   ellipse(x, y, r);
 
 
-  if (x >= width - r / 2.0 || x - r / 2.0 <= 0)
-   { sx = -sx;}
-  if (y >= height - r / 2.0 || y - r / 2.0 <= 0)
-   { sy = -sy;}
+  if (x >= width - r / 2.0 || x - r / 2.0 <= 0){
+    sx = -sx;
+  }
+  if (y >= height - r / 2.0 || y - r / 2.0 <= 0){
+    sy = -sy;
+  }
+  
   x += sx;
   y += sy;
 
